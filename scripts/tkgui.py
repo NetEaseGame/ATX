4bbd1dbc65edab0aea4adec8ce10fb96dc508066
shengxiang <hzsunshx@corp.netease.com>
2016-03-13 16:42:26 +0800

    add rectangle

--- scripts/tkgui.py
+++ scripts/tkgui.py
@@ -24,6 +24,7 @@ class CropIDE(object):
         self._lastx = 0
         self._lasty = 0
         self._color = 'red'
+        self._image = None
 
     def _init_items(self):
         root = self._root
@@ -32,7 +33,7 @@ class CropIDE(object):
         frm_screen = tk.Frame(root, bg='red')
         frm_screen.grid(column=0, row=1)
 
-        tk.Button(frm_control, text="Refresh").grid(column=0, row=0, sticky=tk.W)
+        tk.Button(frm_control, text="Refresh", command=self._reset).grid(column=0, row=0, sticky=tk.W)
         tk.Button(frm_control, text="Save crop").grid(column=1, row=0, sticky=tk.W)
 
         self.canvas = tk.Canvas(frm_screen, bg="blue", bd=0, highlightthickness=0, relief='ridge')
@@ -41,15 +42,23 @@ class CropIDE(object):
         self.canvas.bind("<B1-Motion>", self._add_line)
         self.canvas.bind("<B1-ButtonRelease>", self._done_stroke)
 
+    def _reset(self):
+        self.canvas.delete('currentline')
+
     def _xy(self, event):
         c = self.canvas
         self._lastx, self._lasty = c.canvasx(event.x), c.canvasy(event.y)
+        print self._lastx, self._lasty
 
     def _add_line(self, event):
+        self._reset()
         c = self.canvas
         x, y = c.canvasx(event.x), c.canvasy(event.y)
-        c.create_line((self._lastx, self._lasty, x, y), fill=self._color, width=5, tags='currentline')
-        self._lastx, self._lasty = x, y
+        c.create_rectangle(self._lastx, self._lasty, x, y, 
+            tags='currentline', width=5)#, fill="blue")
+        c.create_line((self._lastx, self._lasty, x, y), fill=self._color, width=2, tags='currentline', dash=(4, 4))
+        c.create_line((self._lastx, y, x, self._lasty), fill=self._color, width=2, tags='currentline', dash=(4, 4))
+        # self._lastx, self._lasty = x, y
 
     def _done_stroke(self, event):
         self.canvas.itemconfigure('currentline', width=2)
@@ -57,6 +66,7 @@ class CropIDE(object):
     def draw_image(self, image):
         tkimage = ImageTk.PhotoImage(image)
         (width, height) = image.size
+        self._image = tkimage # keep a reference
         self.canvas.config(width=width, height=height)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=tkimage)
 
@@ -67,7 +77,11 @@ class CropIDE(object):
 def main():
     gui = CropIDE('Simple IDE')
     image = Image.open('jurassic_park_kitchen.jpg')
+    # (width, height) = image.size
+    # tkimage = ImageTk.PhotoImage(image)
+    # gui.canvas.create_image(0, 0, anchor=tk.NW, image=tkimage)
     gui.draw_image(image)
+    # gui.canvas.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
     gui.mainloop()
 
 
