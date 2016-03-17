791f1fc9130e36a586cc673dfd810f960b3553b1
codeskyblue <codeskyblue@gmail.com>
2016-03-17 20:58:46 +0800

    add gui

--- atx/__main__.py
+++ atx/__main__.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 
 # USAGE
-# python -matx -s ESLKJXX
+# python -matx gui -s ESLKJXX
 
-# import the necessary packages
-# import threading
-# import Tkinter as Tk
-# from Queue import Queue
 import argparse
 
 from atx import tkgui
 
+def gui(args):
+    tkgui.main(args.serial, host=args.host)
 
 def main():
-    ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     ap.add_argument("-s", "--serial", required=False, help="Android serialno")
     ap.add_argument("-H", "--host", required=False, default='127.0.0.1', help="Android serialno")
+
+    subparsers = ap.add_subparsers()
+    parser_gui = subparsers.add_parser('gui')
+    parser_gui.set_defaults(func=gui)
+
     args = ap.parse_args()
-    tkgui.main(args.serial, host=args.host)
+    args.func(args)
+
 
 if __name__ == '__main__':
     main()
