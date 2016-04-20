b32727c7d8bfddc1137c96f3c42b21d37cad112f
wuxc <wuxc08@gmail.com>
2016-04-20 10:05:16 +0800

    sort ui blocks by bounds area

--- atx/record/android_uihooks.py
+++ atx/record/android_uihooks.py
@@ -38,9 +38,6 @@ def str2bool(v):
 def convstr(v):
     return v.encode('utf-8')
 
-def in_bounds(x, y, b):
-    return b.left<x<b.right and b.top<y<b.bottom
-
 class AndroidLayout(object):
     def __init__(self):
         self.tree = None
@@ -49,7 +46,7 @@ class AndroidLayout(object):
 
     def find_clickable_rect(self, x, y):
         def _find(node, x, y):
-            if node.enabled and in_bounds(x, y, node.bounds):
+            if node.enabled and node.bounds.is_inside(x, y):
                 for n in node.children[::-1]:
                     subnode = _find(n, x, y)
                     if subnode and subnode.clickable:
@@ -57,10 +54,14 @@ class AndroidLayout(object):
                 return node
         return _find(self.tree, x, y)
 
+    def find_minimal_clickable_rect(self, x, y):
+        for n in self.nodes:
+            if n.enabled and n.clickable and n.bounds.is_inside(x, y):
+                return n
 
     def find_scrollable_rect(self, x, y):
         for n in self.nodes:
-            if n.enabled and n.scrollable and in_bounds(x, y, n.bounds):
+            if n.enabled and n.scrollable and n.bounds.is_inside(x, y):
                 return n
 
     def display(self):
@@ -79,40 +80,40 @@ class AndroidLayout(object):
             cv2.rectangle(img, (b.left, b.top), (b.right, b.bottom), (83, min(255, i*5), 18), 2)
             i += 1
 
-        global down, move
-        down = None
-        move = False
         def on_mouse(event, x, y, flags, param):
-            global down, move
+            highlight, down, move = param
             if event == cv2.EVENT_LBUTTONDOWN:
-                down, move = (x, y), False
+                param[1] = (x, y)
+                param[2] = False
                 return
             if event == cv2.EVENT_MOUSEMOVE:
-                if move: return
-                if down is None:
-                    move = False
+                if param[2]: return
+                if param[1] is None:
+                    param[2] = False
                     return
-                _x, _y = down
+                _x, _y = param[1]
                 if (_x-x)**2 + (_y-y)**2 > 64:
-                    move = True
+                    param[2] = True
                 return
             if event != cv2.EVENT_LBUTTONUP: return
-            if down and move: # drag
+            param[0][:,:,:] = 0
+            if param[1] and param[2]: # drag
                 node = self.find_scrollable_rect(x*2, y*2)
                 if node:
                     b = node.bounds
                     print 'scroll to', x*2, y*2, b, node.depth
-                    cv2.rectangle(highlight, (b.left, b.top), (b.right, b.bottom), (0,255,255), 4)
+                    cv2.rectangle(param[0], (b.left, b.top), (b.right, b.bottom), (0,255,255), 4)
             else:
-                node = self.find_clickable_rect(x*2, y*2)
+                # node = self.find_clickable_rect(x*2, y*2)
+                node = self.find_minimal_clickable_rect(x*2, y*2)
                 if node:
                     b = node.bounds
                     print 'click at', x*2, y*2, b, node.depth
-                    cv2.rectangle(highlight, (b.left, b.top), (b.right, b.bottom), (0,0,255), 4)
-            down, move = None, False
+                    cv2.rectangle(param[0], (b.left, b.top), (b.right, b.bottom), (0,0,255), 4)
+            param[1], param[2] = None, False
 
         cv2.namedWindow("layout")
-        cv2.setMouseCallback('layout', on_mouse)
+        cv2.setMouseCallback('layout', on_mouse, [highlight, None, False])
 
         while True:
             try:
@@ -188,7 +189,8 @@ class AndroidLayout(object):
 
         self.nodes = []
         self.tree = walk(root, self.nodes)
-        self.nodes.sort(key=lambda x: x.depth, reverse=True)
+        # self.nodes.sort(key=lambda x: x.depth, reverse=True)
+        self.nodes.sort(key=lambda x: x.bounds.area)
 
 if __name__ == '__main__':
     layout = AndroidLayout()
