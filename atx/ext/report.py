ae490ccbaad60d87254f6dd1aa5695f5ca857e52
codeskyblue <codeskyblue@gmail.com>
2016-06-13 22:36:40 +0800

    update report.py but not yet tested, maybe caught something wrong

--- atx/ext/report.py
+++ atx/ext/report.py
@@ -22,8 +22,101 @@ class Report(object):
     # TODO(ssx): move listen to class Report
     """
     def __init__(self, d, save_dir='report'):
+        image_dir = os.path.join(save_dir, 'images')
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+
         self.d = d
-        listen(d, save_dir)
+        self.save_dir = save_dir
+        self.steps = []
+        listen(self.d, self.save_dir)
+        # self.start_record()
+
+    def start_record(self):
+        self.start_time = time.time()
+
+        w, h = self.d.display
+        if self.d.rotation in (1, 3): # for horizontal
+            w, h = h, w
+        result = dict(device=dict(
+            display=dict(width=w, height=h),
+            serial=self.d.serial,
+            start_time=time.strftime("%Y-%m-%d %H:%M:%S"),
+            start_timestamp=time.time(),
+        ), steps=self.steps)
+
+        self.d.add_listener(self.listener, consts.EVENT_ALL ^ consts.EVENT_SCREENSHOT)
+        atexit.register(_finish)
+
+    def _finish(self):
+        data = json.dumps(result)
+        tmpl_path = os.path.join(__dir__, 'index.tmpl.html')
+        save_path = os.path.join(save_dir, 'index.html')
+        json_path = os.path.join(save_dir, 'result.json')
+
+        with open(tmpl_path) as f:
+            html_content = f.read().replace('$$data$$', data)
+
+        with open(json_path, 'wb') as f:
+            f.write(json.dumps(result, indent=4))
+
+        with open(save_path, 'wb') as f:
+            f.write(html_content)
+
+    def add_text(self, text):
+        self.steps.append({
+            'time': '%.1f' % (time.time()-start_time,),
+            'action': 'text',
+            'message': text,
+            'success': True,
+        })
+
+    def listener(self, evt):
+        d = self.d
+        start_time = self.start_time
+        screen_before = 'images/before_%d.png' % time.time()
+        screen_before_abspath = os.path.join(self.save_dir, screen_before)
+
+        if evt.flag == consts.EVENT_CLICK:
+            if evt.depth > 1: # base depth is 1
+                return
+            d.last_screenshot.save(screen_before_abspath)
+            screen_after = 'images/after_%d.png' % time.time()
+            d.screenshot(os.path.join(self.save_dir, screen_after))
+
+            (x, y) = evt.args
+            self.steps.append({
+                'time': '%.1f' % (time.time()-start_time,),
+                'action': 'click',
+                'screen_before': screen_before,
+                'screen_after': screen_after,
+                'position': {'x': x, 'y': y},
+                'success': True,
+            })
+        elif evt.flag == consts.EVENT_CLICK_IMAGE:
+            step = {
+                'time': '%.1f' % (time.time()-start_time,),
+                'action': 'click_image',
+                'success': evt.traceback is None,
+                'traceback': None if evt.traceback is None else evt.traceback.stack,
+            }
+            if d.last_screenshot:
+                d.last_screenshot.save(screen_before_abspath)
+                step['screen_before'] = screen_before
+            if evt.traceback is None or not isinstance(evt.traceback.exception, IOError):
+                target = 'images/target_%d.png' % time.time()
+                target_abspath = os.path.join(self.save_dir, target)
+                pattern = d.pattern_open(evt.args[0])
+                pattern.save(target_abspath)
+                step['target'] = target
+            if evt.traceback is None:
+                screen_after = 'images/after_%d.png' % time.time()
+                d.screenshot(os.path.join(self.save_dir, screen_after))
+                step['screen_after'] = screen_after
+                step['confidence'] = evt.retval.confidence
+                (x, y) = evt.retval.pos
+                step['position'] = {'x': x, 'y': y}
+            self.steps.append(step)
 
 
 def listen(d, save_dir='report'):
