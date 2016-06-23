51d5783e2b2f89d7c9e4b178cb899d07ff6ce648
codeskyblue <codeskyblue@gmail.com>
2016-06-23 10:03:43 +0800

    remove uesless comment

--- atx/device/ios/bootstrap.sh
+++ atx/device/ios/bootstrap.sh
@@ -1,43 +1,37 @@
 #!/bin/bash -
 #
 set -e
-cd $(dirname $0)
+CWD=$(cd $(dirname $0); pwd)
 
 BUNDLE_ID=${BUNDLE_ID:?}
 UDID=${UDID:-$(idevice_id -l)}
-TEST="./instruments-test.js"
-PIPE=${PIPE:-"/tmp/atx.instruments.$UDID.pipe"}
+TEST="$CWD/instruments-test.js"
 
 TRACETEMPLATE="/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate"
 
 QUEUE="python -m atx.taskqueue --room $UDID"
 #echo "UDID: $UDID"
-# test -p "$PIPE" || mkfifo "$PIPE"
 
-#RESULTPATH=results-$UDID
 test -d $RESULTPATH || mkdir $RESULTPATH
 case "$1" in
 	instruments)
 		# python -m atx.taskqueue web &>/tmp/atx.taskqueue.log &
-		exec instruments -w ${UDID:?} -t "$TRACETEMPLATE" $BUNDLE_ID -e UIASCRIPT $TEST # -e UIARESULTSPATH $RESULTPATH
+		exec instruments -w ${UDID:?} -t "Automation" -D cli.trace $BUNDLE_ID -e UIASCRIPT $TEST # -e UIARESULTSPATH $RESULTPATH
+		#exec instruments -w ${UDID:?} -t "$TRACETEMPLATE" $BUNDLE_ID -e UIASCRIPT $TEST # -e UIARESULTSPATH $RESULTPATH
 		;;
 	run)
 		shift
 		TASK_ID=$($QUEUE post "$1")
 		exec $QUEUE delete "$TASK_ID"
-		# /bin/echo "$@" >> $PIPE
-		# exec /usr/bin/head -n1 $PIPE
 		;;
 	get)
 		shift
 		$QUEUE get
-		# /usr/bin/head -n1 $PIPE
 		;;
 	put)
 		shift
 		# $1: task_id, $2: data
 		$QUEUE put "$1" "$2"
-		# /bin/echo "$@" >> $PIPE
 		;;
 	reset)
 		# todo, maybe quit too much
